43d62dba7d6221fe446256c46d8d7ec426ddbc89
erikamaecapua1 <erika.mae.n.capua@accenture.com>
2025-12-07 20:43:11 +0000

    refactor: move tracker app to backend, update settings and urls

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'octofit_tracker.tracker',
+    'tracker',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
